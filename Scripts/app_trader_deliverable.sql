0041712c40deb263ab3962283af7c2f134d23ccc
RenukaD13 <renuka.rs.deshmukh@gmail.com>
2024-10-15 19:31:55 -0500

    Created new renuka_branch

---
+++ Scripts/app_trader_deliverable.sql
@@ -0,0 +1,6 @@
+/*3. Deliverables
+a. Develop some general recommendations as to the price range, genre, content rating, or anything else for apps that the company should target.
+
+b. Develop a Top 10 List of the apps that App Trader should buy.
+
+c. Submit a report based on your findings. All analysis work must be done using PostgreSQL, however you may export query results to create charts in Excel for your report.*/
