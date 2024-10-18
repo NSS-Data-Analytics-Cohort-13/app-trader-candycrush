2383d1da67a1fdbb37d774ab71a84ae7747d5bf5
abidali1988 <abid_skt@yahoo.com>
2024-10-17 21:37:18 -0500

    still running queries

--- Script/script.sql
+++ Script/script.sql
@@ -12,8 +12,7 @@ WHERE a.rating >= 4.5 AND p.rating >= 4.5
 ORDER BY a.rating DESC, p.rating DESC
 
 
-SELECT 
-    apple_store.Name,  
+SELECt apple_store.Name,  
     CASE 
         WHEN apple_store.price <= 1.00 THEN 10000 
         ELSE apple_store.price * 10000 
@@ -23,6 +22,36 @@ FROM app_store_apps AS apple_store
 	INNER JOIN  play_store_apps AS play_store 
 		ON apple_store.Name = play_store.Name
 ORDER BY AverageRating DESC
+LIMIT 10;
+
+SELECT
+    apple_store.Name,
+    CASE
+        WHEN apple_store.price <= 1.00 THEN 10000
+        ELSE apple_store.price * 10000
+    END AS PurchasePrice
+FROM app_store_apps AS apple_store
+--WHERE apple_store.name = 'Toca Life: City'
+UNION
+SELECT
+    play_store.Name,
+    CASE
+        WHEN CAST(TRIM(REPLACE(play_store.price,'$','')) AS DECIMAL) <= 1.00 THEN 10000
+        ELSE CAST(TRIM(REPLACE(play_store.price,'$','')) AS DECIMAL) * 10000
+    END AS PurchasePrice
+FROM play_store_apps AS play_store
+--WHERE play_store.name = 'Toca Life: City'
+ORDER BY PurchasePrice DESC
+
+SELECT distinct(a.name), SUM(CAST(a.review_count AS INTEGER) + p.review_count) AS total_review, AVG(a.rating + p.rating) AS avg_rating,
+	CASE WHEN a.price >= CAST(REPLACE(p.price,'$','') AS DECIMAL) THEN a.price
+	ELSE CAST(REPLACE(p.price,'$','') AS DECIMAL) END AS high_price
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+GROUP BY a.name, a.price, p.price
+ORDER BY total_review DESC
+
 LIMIT 10;
 
 
